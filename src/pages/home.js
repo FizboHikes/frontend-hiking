efcf859d5fb3f7351dfb247519e4a9f626b5b112
learn <learn@learn08.localdomain>
2018-12-04 10:33:20 -0800

    added login and register fields

---
+++ src/pages/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Register from '../components/register'
+import Login from '../components/login'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+      home page
+      <Login/>
+      <Register/>
+      </div>
+    );
+  }
+}
+
+export default Home;
