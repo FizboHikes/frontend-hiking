d3b505dc3293722137398eec8f54def7c93bb8f9
learn <learn@learn08.localdomain>
2018-12-05 17:05:53 -0800

    started hike API

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,12 +3,46 @@ import Home from '../pages/home'
 import NavMenu from '../components/navmenu'
 
 class Dashboard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      url: "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=7039084-3fb0c833662f3c90057cc37e292d1118",
+      hikeList: undefined
+    }
+  }
+
+  componentDidMount(){
+    fetch(this.state.url)
+    .then((stringResponse) => {
+      return stringResponse.json()
+    })
+    .then((json) => {
+      let {trails} = json
+      this.setState({hikeList: trails})
+    })
+  }
+
+
+
   render() {
+    if(this.state.hikeList){
     return (
       <div>
-        Sup this is the dashboard.
+        {this.state.hikeList[4].name}
+        <br/>
+        {this.state.hikeList[4].location}
+
+        <br/>
+        {this.state.hikeList[4].conditionDate}
+        <br/>
+        {this.state.hikeList[4].conditionDetails}
       </div>
     );
+  } else {
+    return(
+      <p>loading</p>
+    )
+  }
   }
 }
 
